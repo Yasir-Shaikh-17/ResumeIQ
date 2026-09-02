0eea4ca527814b898065bfd42df36db803534705
Yasir Shaikh <yasirshaikh.1610@gmail.com>
2026-09-02 10:49:53 +0500

    URL bug resolve

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,14 +2,18 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const authRouter = require("./routes/auth.routes");
-const interviewRouter = require("./routes/interview.routes")
+const interviewRouter = require("./routes/interview.routes");
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
-
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://resume-iq-nine-sigma.vercel.app"],
+    credentials: true,
+  }),
+);
 app.use("/api/auth", authRouter);
-app.use("/api/interview", interviewRouter)
+app.use("/api/interview", interviewRouter);
 
 module.exports = app;
